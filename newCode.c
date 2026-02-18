f4f6973840cf3956e241c392328130014f7b3fb3
SebastianUVG <gar22291@uvg.edu.gt>
2026-02-17 20:03:37 -0600

    ejemplo de otro lenguaje

---
+++ newCode.c
@@ -0,0 +1,11 @@
+#include <stdio.h>/*Comentario multilínea*/
+int main() {
+    int entero = 50 // Programa de prueba en C
+    float flotante = 3.14
+    double cientifico = 6.02e23
+    int hexadecimal = 0xFF
+    if (entero >= 10 && flotante != 0.0) {
+        printf("Hola\tMundo\n")
+    }
+    return 0
+}
